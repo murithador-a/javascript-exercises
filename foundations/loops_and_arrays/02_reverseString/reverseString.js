32cd78d5f2793d6f9cc0f496880e1525ef4591a8
murithador-a <codewithmurking@gmail.com>
2026-09-12 20:25:05 +0100

    loops and array 02

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function(string) {
-  return string.split("Hello Murking").reverse().join("Murking")
-};
+const reverseString = function(string){
+  let result = "";
+
+  for (let i = string.length - 1; i>=0; i--){
+    result += string[i];
+  }
+
+  return result;
+}
+ 
 
 // Do not edit below this line
 module.exports = reverseString;
