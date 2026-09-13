e4f709d9ab12eb0babdbb36509a24506d1afd93e
murithador-a <codewithmurking@gmail.com>
2026-09-13 06:56:37 +0100

    removeFromArray

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+
+  for (let item of items) {
+    let index = array.indexOf(item);
+    array.splice(index, 1);
+  }
+
+  return array;
 };
 
 // Do not edit below this line
